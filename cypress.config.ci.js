6ed70240ba0ba32c8ee11e9086b0315e2dd02939
o-sahaidak <o.sahaidak1@gmail.com>
2026-05-09 02:51:04 +0300

    add separate ci config file

---
+++ cypress.config.ci.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://telnyx.com',
+    chromeWebSecurity: false,
+    numTestsKeptInMemory: 1,
+    experimentalMemoryManagement: true,
+    projectId: "lzb1rS",
+    video: true,
+    screenshotOnRunFailure: true,
+  },
+})
