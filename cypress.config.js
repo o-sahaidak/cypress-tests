126b063e117a08a57edae0ee9d4092abfe345302
o-sahaidak <o.sahaidak1@gmail.com>
2026-05-09 03:01:18 +0300

    fixed separate ci config file

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,6 @@ module.exports = defineConfig({
     chromeWebSecurity: false,
     numTestsKeptInMemory: 1,
     experimentalMemoryManagement: true,
-    projectId: "lzb1rS",
+    projectId: "izbir5",
   },
 })
